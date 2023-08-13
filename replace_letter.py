30fea163378936d8838ed5b3215fb19f9b061709
ansalr <ansalr.email@gmail.com>
2023-08-13 16:40:05 +0530

    program updation in replace_letter.py

--- replace_letter.py
+++ replace_letter.py
@@ -49,3 +49,18 @@ ch2 = 'p'
 replace(s,ch1,ch2)
 
 # paales
+
+
+"""
+def swap (user_input, str1, str2):
+    result = ''
+    if user_input != None:
+        result = user_input.replace(str1, '*').replace(str2, str1).replace('*', str2)
+        return result
+    return 'Null'
+user_input = input()
+str1, str2 = map(str,input().split())
+print(swap(user_input, str1,str2))
+
+
+"""
