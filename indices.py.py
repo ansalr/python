576706ca9424d8e622beaab33b39764467c40ce9
ansalr <ansalr.email@gmail.com>
2023-08-13 15:25:36 +0530

    delete py_program file  move file

--- indices.py.py
+++ indices.py.py
@@ -4,13 +4,12 @@ def sort_numbers_with_indices(numbers):
     original_indices = [x[0] for x in sorted_indices]
     return sorted_numbers, original_indices
 
-# Sample input
+# input
 numbers = [7, 3, 10, 5, 2]
 
-# Sorting the numbers while keeping track of original indices
+
 sorted_numbers, original_indices = sort_numbers_with_indices(numbers)
 
-# Displaying the sorted numbers and original indices
 for index, value in enumerate(sorted_numbers):
     original_index = original_indices[index]
     print(f"Number: {value}, Original Index: {original_index} ")
