9ef0fd46ebe929828282fbbcc08c89c7712a57d1
ansalr <ansalr.email@gmail.com>
2023-08-17 21:38:32 +0530

    updated intrest_rate.py file

--- intrest_rate.py
+++ intrest_rate.py
@@ -1,10 +1,23 @@
-p = int(input())
-year = int (input())
-sum = 0
-instalment = int(input())
-year,mir = map(float,input().split())
-square_value = (1+mir)**(year*12)
-div = 1-1/square_value
-mult = p*mir
-emi = mult/div
-print(emi)
+def intrest_rate():
+    bank = []
+    p = int(input())
+    year = int (input())
+    for i in range(2):
+        sum = 0
+        instalment = int(input())
+        for j in range(instalment):
+            year,mir = map(float,input().split())
+            square_value = (1+mir)**(year*12)
+            div = 1-1/square_value
+            mult = p*mir
+            emi = mult/div
+            sum +=emi
+        bank.append(sum)
+    
+    if bank[0]<bank[1]:
+        return "Bank A"
+    else:
+        return "Bank B"
+    
+output = intrest_rate()
+print(output)
