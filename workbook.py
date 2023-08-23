78814ef3b335f0dd453eace9c47fa96e30247ad2
ansalr <ansalr.email@gmail.com>
2023-08-23 11:33:37 +0530

    reset workbook file

--- workbook.py
+++ workbook.py
@@ -1,13 +1,8 @@
-arr = [345,604,321,433,704,470,808,718,517,811]
-empty = []
-
-for i in range(2):
-        count = 0  
-        len_arr,r_range = map(int,input().split())
-        for val in arr:
-            
-            if (val>=len_arr and val<=r_range):
-                        count+=1
-        empty.append(count)
-
-print(empty)
+number = input()
+rev_num = str(number)[::-1]
+if type(number)==int:
+    print(int(rev_num))
+elif type(number)==float:
+    print(int(rev_num)) 
+else:
+    print(rev_num)
