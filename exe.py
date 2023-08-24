ff02253a1a0485f1664cf2a34203ac29cec512a7
Ansal <65532075+ansalr@users.noreply.github.com>
2023-08-24 14:39:17 +0530

    backup

---
+++ exe.py
@@ -0,0 +1,11 @@
+n = 5
+width = 9
+j = 1
+for i in range(1,n+1):
+    p_h = "H"*j
+    print(p_h.center(width," "))
+    width-+1
+    j+=2
+
+
+"https://www.hackerrank.com/challenges/text-alignment/problem?isFullScreen=true"
