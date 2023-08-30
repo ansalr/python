4b3fef574db33872c515c4c9a0b844ded5abab86
ansalr <ansalr.email@gmail.com>
2023-08-30 22:21:36 +0530

    updated hcf fun day2.py

--- day2.py
+++ day2.py
@@ -350,3 +350,18 @@ def dictionary()
     for details in student:    
         print(details,student[details],sep=" : ")
 # dictionary()
+
+def hcf():
+    num1,num2 = map(int,input().split())
+    
+    i = 1
+    while ( i < num1 and i < num2 ):
+    
+        if ( num1%i == 0 == num2%i ):
+    
+            hcf = i
+        i +=1
+    
+    print(hcf)
+
+# hcf()
