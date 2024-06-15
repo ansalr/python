3c8b74d95cc0b8051cfd7c0705a5512937f15e14
ansalr <ansalr.email@gmail.com>
2024-06-15 11:09:51 +0530

    clean v2e dir

--- V2e/test.py
+++
@@ -1,9 +0,0 @@
-import re
-# n= "ad768D&"
-n = input("Enter string: ")
-pattern = re.compile('''^(?=.*[a-z]{2,5})(?=.*[0-9]{3,4})(?=.*[A-Z]{1,2})(?=.*[!@#$%^&*]{1,2}).{5,10}$''')
-
-if pattern.match(n):
-    print("Valid")
-else:
-    print("not valid")
